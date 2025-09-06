8fad78d5fec54152cb5d0cd5a6b5a0158edd634a
Mthuthuzeli Paul Ndwendwa <102731341+gridX777@users.noreply.github.com>
2025-09-06 05:31:02 -0700

    Add files via upload

---
+++ LoginFrameApp.java
@@ -0,0 +1,13 @@
+package loginframeapp;
+
+import za.ac.tut.ui.LoginFrame;
+
+
+public class LoginFrameApp {
+
+
+    public static void main(String[] args) {
+       new LoginFrame();
+    }
+    
+}
